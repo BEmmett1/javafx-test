342ff592f47e9581388786ebca319f51b255eced
Benjamin Thompson <benny@thompsonmail.org>
2024-04-02 18:58:57 -0400

    init commit

---
+++ demo/src/main/java/com/example/App.java
@@ -0,0 +1,42 @@
+package com.example;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.image.Image;
+
+import java.io.IOException;
+
+/**
+ * JavaFX App
+ */
+public class App extends Application {
+
+    private static Scene scene;
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        scene = new Scene(loadFXML("primary"), 640, 480);
+        //stage.getIcons().remove(0);
+        stage.getIcons().add(new Image("https://vectorflags.s3.amazonaws.com/flags/us-square-01.png", 64, 32, false, false));
+        stage.setScene(scene);
+        stage.setResizable(true);
+        stage.show();
+    }
+
+    static void setRoot(String fxml) throws IOException {
+        scene.setRoot(loadFXML(fxml));
+    }
+
+    private static Parent loadFXML(String fxml) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
+        return fxmlLoader.load();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+}
